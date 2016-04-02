8946377be5eb9d462d991e298d47c67477e8b921
ShaoJiansheng <shaojiansheng@shaojiaengdembp.lan>
2016-04-02 23:58:12 +0800

    创建项目

---
+++ lifeCircle/lifeCircle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lifeCircle
+//
+//  Created by ShaoJiansheng on 16/4/2.
+//  Copyright © 2016年 ASM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lifeCircle/lifeCircle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lifeCircle
+//
+//  Created by ShaoJiansheng on 16/4/2.
+//  Copyright © 2016年 ASM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
